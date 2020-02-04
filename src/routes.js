a19b9727a2f16eca4831fcf6dab89f1e65779b62
Lucas Siqueira <lucassiqzro@gmail.com>
2020-02-03 23:17:54 -0300

    Configure multer to file upload

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionControler from './app/controllers/SessionController';
 import RecipientController from './app/controllers/RecipientController';
 import DeliverymanController from './app/controllers/DeliverymanController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -13,6 +16,8 @@ import validadeRecipientStore from './app/validators/RecipientStore';
 import valdiadeDeliverymanStore from './app/validators/DeliverymanStore';
 
 const routes = new Router();
+const upload = multer(multerConfig);
+
 
 routes.post('/users', validadeUserStore, UserController.store);
 routes.post('/session', validadeSessionStore, SessionControler.store);
@@ -20,7 +25,10 @@ routes.post('/session', validadeSessionStore, SessionControler.store);
 routes.use(authMiddleware);
 
 routes.post('/recipients', validadeRecipientStore, RecipientController.store);
+
 routes.post('/deliverymans', valdiadeDeliverymanStore,DeliverymanController.store);
 routes.get('/deliverymans', DeliverymanController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
